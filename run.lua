f72fbf2ad2bfa74f75052289c7cadf45d2940b67
AlphaTechNinja <114359889+AlphaTechNinja@users.noreply.github.com>
2023-05-09 19:39:00 -0500

    Update run.lua
    Adds the ability to include dictionaries and files

--- run.lua
+++ run.lua
@@ -1,5 +1,6 @@
 --runs a file
-local path = ...
+local args = {...}
+local path = table.remove(args,1)
 local file = fs.open(path,"r")
 local code = file.readAll()
 print("data:")
@@ -8,6 +9,16 @@ file.close()
 local cmds = dofile("wip/commands.lua")
 --flush file
 cmds.flush(code)
+if #args > 0 then print("included:") end
+for i,v in ipairs(args) do
+if fs.isDir(v) then
+print(v.." (dir)")
+cmds.execute("lib "..v)
+else
+print(v.." (file)")
+cmds.execute("include "..v)
+end
+end
 --run loop
 print("main:")
 cmds.run()
