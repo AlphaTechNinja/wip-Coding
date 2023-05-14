f96f637b7173d816a906fab1f0993b779cf98d95
AlphaTechNinja <114359889+AlphaTechNinja@users.noreply.github.com>
2023-05-14 15:08:24 -0700

    Add files via upload

---
+++ debug.lua
@@ -0,0 +1,40 @@
+--debug version
+--points to the fail location
+--runs a file
+local args = {...}
+local path = table.remove(args,1)
+local file = fs.open(path,"r")
+local code = file.readAll()
+print("data:")
+print(code)
+file.close()
+local cmds = dofile("wip/commands.lua")
+--flush file
+cmds.flush(code)
+if #args > 0 then print("included:") end
+for i,v in ipairs(args) do
+if fs.isDir(v) then
+print(v.." (dir)")
+cmds.execute("lib "..v)
+else
+print(v.." (file)")
+cmds.execute("include "..v)
+end
+end
+--run loop
+print("main:")
+local _,err = pcall(cmds.run)
+if err then
+local errpos = cmds.cpu.pos - 1
+print("error:")
+print("message:")
+print(err)
+print("debug:")
+for i,v in ipairs(cmds.cpu.buffer) do
+if i == errpos then
+print(v..":<-- here")
+else
+print(v..":")
+end
+end
+end
